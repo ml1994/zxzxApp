c22ef76878aac9cd87c2f59b11dae88e9515f828
Root <940150534@qq.com>
2017-09-17 19:12:58 +0800

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit 3b1f410bf713c050ad2acf206e49116a561204bb, reversing
    changes made to 9773b365477b049b75fc4cedac49d4f27adb1818.

--- app/views/login.js
+++ app/views/login.js
@@ -5,81 +5,26 @@ import Input from '../components/input'
 import {connect} from 'react-redux'
 import {NavigationActions} from 'react-navigation'
 
-import * as userinfoActions from '../actions/userinfo'
-
 class Login extends Component {
 
-	constructor(props){
-		super(props)
-		const {dispatch} = this.props
-		dispatch(userinfoActions.initLogin())
-		this.state = {
-			phoneNum:'',
-			psw:''
-		}
-	}
-
-	asyLogin(payload){
-		const {dispatch} = this.props
-		const {phoneNum,psw} = this.state
-		
-		return dispatch=>{
-			dispatch({type:'LOGING'})
-			fetch('http://115.236.94.196:30005/app/account/login',{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-				},
-				body: `phone=${phoneNum}&password=${psw}`
-			})
-			.then(res=>{
-				return res.json()
-			})
-			.then(resj=>{
-				//console.log(resj)
-				const {code,message} = resj
-				if(code==0){
-					dispatch(userinfoActions.login(payload))
-					dispatch(NavigationActions.back())
-				}else{
-					alert(message)
-				}
-			})
-			.catch(err=>{
-				console.log(err)
-				dispatch({type:'LOGIN_ERROR'})
-			})
-		}
-	}
-
-	loginFun(){
-		const userinfo = this.state
-		const {dispatch} = this.props
-		dispatch(this.asyLogin(userinfo))
-	}
-
 	render() {
-		const {dispatch,userinfo}=this.props
-
+		const {dispatch}=this.props
 		return (
 			<View style={styles.rootView}>
-				<Header type='title' title='登录' isLoginPage={true}/>
+				<Header type='title' title='登录'/>
 				<Image resizeMode='contain' source={require('../asset/logo.png')} style={styles.logo}/>
-				<Input iconLeft='mobile' placeholder='请输入手机号码' onChangeText={phoneNum=>this.setState({phoneNum})}/>
-				<Input iconLeft='lock' type='password' placeholder='请输入密码6-16位' onChangeText={psw=>this.setState({psw})}/>
-				<TouchableOpacity style={styles.btnSubmit} onPress={()=>this.loginFun()}>
+				<Input iconLeft='mobile' placeholder='请输入手机号码'/>
+				<Input iconLeft='lock' placeholder='请输入密码6-16位'/>
+				<TouchableOpacity style={styles.btnSubmit}>
 					<Text style={styles.btnText}>登录</Text>
 				</TouchableOpacity>
 				<View style={styles.managerView}>
+					<Text style={styles.managerText}>没账号?去</Text>
 					<TouchableOpacity onPress={() => dispatch(NavigationActions.navigate({routeName:'Register'}))}>
-						<View style={[styles.managerTextView,styles.managerTextSplice]}>
-							<Text style={styles.managerText}>没账号?去注册</Text>
-						</View>
+						<Text style={[styles.managerText,styles.managerTextSplice]}>注册</Text>
 					</TouchableOpacity>
 					<TouchableOpacity onPress={() => dispatch(NavigationActions.navigate({routeName:'ForgetPassword'}))}>
-						<View style={styles.managerTextView}>
-							<Text style={styles.managerText}>忘记密码</Text>
-						</View>
+						<Text style={styles.managerText}>忘记密码</Text>
 					</TouchableOpacity>
 				</View>
 			</View>
@@ -111,29 +56,23 @@ const styles = StyleSheet.create({
 		lineHeight: 20
 	},
 	managerView: {
+		flex: 1,
 		flexDirection: 'row',
 		marginTop:20
 	},
 	managerText: {
-		color: '#0f1d4e',
-		
-	},
-	managerTextView:{
-		justifyContent:'center',
-		height:14
+		color: '#0f1d4e'
 	},
 	managerTextSplice: {
 		borderRightWidth: 1,
 		borderRightColor: '#0f1d4e',
 		paddingRight:5,
-		marginRight:5,
+		marginRight:5
 	}
-
 })
 const mapStateToProps=store=>{
 	return {
-		nav:store.nav,
-		userinfo:store.userinfo
+		nav:store.nav
 	}
 }
 
