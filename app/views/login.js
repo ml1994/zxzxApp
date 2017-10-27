dd62548169c982b9e684ee6e3fb814ed0cb42797
Root <940150534@qq.com>
2017-10-28 06:51:28 +0800

    解决键盘遮挡输入框问题

--- app/views/login.js
+++ app/views/login.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Text, StyleSheet, Image, TouchableOpacity,Alert} from 'react-native'
+import {View, Text, StyleSheet, Image, TouchableOpacity, Alert, KeyboardAvoidingView} from 'react-native'
 import {connect} from 'react-redux'
 import {NavigationActions} from 'react-navigation'
 import JPushModule from 'jpush-react-native'
@@ -14,59 +14,59 @@ import * as testActions from '../actions/test'
 
 class Login extends Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props)
 		const {dispatch} = this.props
 		dispatch(userinfoActions.initLogin())
 		this.state = {
-			account:'',
-			psw:''
+			account: '',
+			psw: ''
 		}
 	}
 
-	asyLogin(payload){
+	asyLogin(payload) {
 		const {dispatch} = this.props
-		const {account,psw} = this.state
-		
-		return dispatch=>{
-			dispatch({type:'LOGING'})
+		const {account, psw} = this.state
+
+		return dispatch => {
+			dispatch({type: 'LOGING'})
 			myFetch.post(
 				'/account/login',
 				`phone=${account}&password=${psw}`,
-				resj=>{
-					const {code,message} = resj
-					if(code==0){
+				resj => {
+					const {code, message} = resj
+					if (code == 0) {
 						this.postJPushRegistrationId()//发送registrationId
 						dispatch(userinfoActions.login(payload))
 						dispatch(this.asyGetUserinfo())//获取用户信息
 						dispatch(this.initMaxScore())//获取最高分
 						dispatch(this.getList())//获取问题列表
 						dispatch(NavigationActions.back())
-					}else{
-						Alert.alert('提示',message)
+					} else {
+						Alert.alert('提示', message)
 					}
 				},
-				err=>{
+				err => {
 					console.log(err)
-					dispatch({type:'LOGIN_ERROR'})
+					dispatch({type: 'LOGIN_ERROR'})
 				}
 			)
 		}
 	}
 
-	initMaxScore(){//获取最高分
+	initMaxScore() {//获取最高分
 		const {dispatch} = this.props
 		let maxScore = new Array()
-		return dispatch=>{
-			dispatch({type:'GETTING_MAX_SCORE'})
+		return dispatch => {
+			dispatch({type: 'GETTING_MAX_SCORE'})
 			myFetch.get(
 				'/examqueBank/list',
 				{},
 				res => {
-					res.rows.map((item)=>{
+					res.rows.map((item) => {
 						maxScore.push(item.maxscore);
 					})
-					console.log(res,maxScore)
+					console.log(res, maxScore)
 					dispatch(testActions.getMaxScore({maxScore}))
 				},
 				err => {
@@ -76,137 +76,139 @@ class Login extends Component {
 		}
 	}
 
-	getList(){//获取问题列表
-        const {dispatch} = this.props
-        return dispatch=>{
-            //dispatch(appStateActions.fetch({fetching:true}))
-            myFetch.get(
-                '/consult/list/question',
-                {page:1,pagesize:1000},
-                res=>{
-                    console.log(res)
-                    if(res.code==0){
-                        if(res.data.question.rows.length!=0){
-                            let vip = ''
-                            let partner = ''
-                            if(res.data.partner){
-                                partner = res.data.partner.tails.partner_name
-                                vip = true
-                            }else{
-                                vip = false
-                            }
-                            const askList = res.data.question.rows
-                            dispatch(userinfoActions.getInfo({
-                                account:askList[0].tails.account,
-                                vip,
-                                partner
-                            }))
-                            dispatch(askActions.loadAskList({askList}))
-                            dispatch({type:'LOADED_ASKLIST'})
-                        }else{
-                            dispatch({type:'NO_ASKLIST'})
-                        }
-                    }
-                },
-                err=>{
-                    console.log(err)
-                    Alert.alert('提示','获取列表失败')
-                }
-            )
-        }
-    }
+	getList() {//获取问题列表
+		const {dispatch} = this.props
+		return dispatch => {
+			//dispatch(appStateActions.fetch({fetching:true}))
+			myFetch.get(
+				'/consult/list/question',
+				{page: 1, pagesize: 1000},
+				res => {
+					console.log(res)
+					if (res.code == 0) {
+						if (res.data.question.rows.length != 0) {
+							let vip = ''
+							let partner = ''
+							if (res.data.partner) {
+								partner = res.data.partner.tails.partner_name
+								vip = true
+							} else {
+								vip = false
+							}
+							const askList = res.data.question.rows
+							dispatch(userinfoActions.getInfo({
+								account: askList[0].tails.account,
+								vip,
+								partner
+							}))
+							dispatch(askActions.loadAskList({askList}))
+							dispatch({type: 'LOADED_ASKLIST'})
+						} else {
+							dispatch({type: 'NO_ASKLIST'})
+						}
+					}
+				},
+				err => {
+					console.log(err)
+					Alert.alert('提示', '获取列表失败')
+				}
+			)
+		}
+	}
 
-	asyGetUserinfo(){
-        //获取用户信息
-        const {dispatch} = this.props
-        return dispatch=>{
-            dispatch({type:'GETING'})
-            myFetch.get(
-                '/account/getinfo',
-                {},
-                resj=>{
-                    const {account} = resj.data
-                    let vip = ''
-                    let partner = ''
-                    if(resj.data.tails.vip==true){
-                        vip = resj.data.tails.vip
-                        partner = resj.data.tails.staff.tails.partner_name
-                    }
-                    const info = Object.assign({},{account,vip,partner})
+	asyGetUserinfo() {
+		//获取用户信息
+		const {dispatch} = this.props
+		return dispatch => {
+			dispatch({type: 'GETING'})
+			myFetch.get(
+				'/account/getinfo',
+				{},
+				resj => {
+					const {account} = resj.data
+					let vip = ''
+					let partner = ''
+					if (resj.data.tails.vip == true) {
+						vip = resj.data.tails.vip
+						partner = resj.data.tails.staff.tails.partner_name
+					}
+					const info = Object.assign({}, {account, vip, partner})
 					dispatch(userinfoActions.getInfo(info))
-					dispatch({type:'GETING_END'})
-                },
-                err=>{
-                    console.log(err)
-                    dispatch({type:'GET_ERROR'})
-                }
-            )
-        }
-	}
-	
-	postJPushRegistrationId(){
-		JPushModule.getRegistrationID((registrationId) => { //JPush传送registrationId给后台
-            myFetch.post(
-                '/push/reg',
-                `id=${registrationId}`,
-                res=>{
-                    console.log(registrationId,res)
-                },
-                err=>{
-                    console.log(err)
-                }
-            )
-        })
+					dispatch({type: 'GETING_END'})
+				},
+				err => {
+					console.log(err)
+					dispatch({type: 'GET_ERROR'})
+				}
+			)
+		}
 	}
 
-	loginFun(){
+	postJPushRegistrationId() {
+		JPushModule.getRegistrationID((registrationId) => { //JPush传送registrationId给后台
+			myFetch.post(
+				'/push/reg',
+				`id=${registrationId}`,
+				res => {
+					console.log(registrationId, res)
+				},
+				err => {
+					console.log(err)
+				}
+			)
+		})
+	}
+
+	loginFun() {
 		const userinfo = this.state
 		const {dispatch} = this.props
 		dispatch(this.asyLogin(userinfo))
 	}
 
 	render() {
-		const {dispatch,userinfo}=this.props
-		
+		const {dispatch, userinfo} = this.props
+
 		return (
-			<View style={styles.rootView}>
+			<KeyboardAvoidingView style={styles.rootView} behavior="padding">
 				<Header type='title' title='登录' isLoginPage={true}/>
 				<Image resizeMode='contain' source={require('../asset/logo.png')} style={styles.logo}/>
 				<View style={styles.inputsView}>
-					<Input iconLeft='mobile' placeholder='请输入手机号码' onChangeText={account=>this.setState({account})}/>
-					<Input iconLeft='lock' type='password' placeholder='请输入密码6-16位' onChangeText={psw=>this.setState({psw})}/>
+					<Input iconLeft='mobile' placeholder='请输入手机号码' onChangeText={account => this.setState({account})}/>
+					<Input iconLeft='lock' type='password' placeholder='请输入密码6-16位'
+						   onChangeText={psw => this.setState({psw})}/>
 				</View>
-				<TouchableOpacity style={styles.btnSubmit} onPress={()=>this.loginFun()}>
+				<TouchableOpacity style={styles.btnSubmit} onPress={() => this.loginFun()}>
 					<Text style={styles.btnText}>登录</Text>
 				</TouchableOpacity>
 				<View style={styles.managerView}>
-					<TouchableOpacity onPress={() => dispatch(NavigationActions.navigate({routeName:'Register'}))}>
-						<View style={[styles.managerTextView,styles.managerTextSplice]}>
+					<TouchableOpacity onPress={() => dispatch(NavigationActions.navigate({routeName: 'Register'}))}>
+						<View style={[styles.managerTextSplice]}>
 							<Text style={styles.managerText}>没账号?去注册</Text>
 						</View>
 					</TouchableOpacity>
-					<TouchableOpacity onPress={() => dispatch(NavigationActions.navigate({routeName:'ForgetPassword'}))}>
-						<View style={styles.managerTextView}>
-							<Text style={styles.managerText}>忘记密码</Text>
-						</View>
+					<TouchableOpacity
+						onPress={() => dispatch(NavigationActions.navigate({routeName: 'ForgetPassword'}))}>
+						<Text style={styles.managerText}>忘记密码</Text>
 					</TouchableOpacity>
 				</View>
-			</View>
+			</KeyboardAvoidingView>
 		)
 	}
 }
+
 const styles = StyleSheet.create({
 	rootView: {
 		flex: 1,
 		height: '100%',
 		backgroundColor: '#fff',
 		alignItems: 'center',
+		justifyContent: 'center'
 	},
 	logo: {
 		width: 130
 	},
-	inputsView:{
-		width:'80%'
+	inputsView: {
+		width: '80%'
 	},
 	btnSubmit: {
 		width: '80%',
@@ -223,29 +225,25 @@ const styles = StyleSheet.create({
 		lineHeight: 20
 	},
 	managerView: {
+		flex: 1,
 		flexDirection: 'row',
-		marginTop:20
+		marginTop: 20
 	},
 	managerText: {
 		color: '#0f1d4e',
-		
-	},
-	managerTextView:{
-		justifyContent:'center',
-		height:14
 	},
 	managerTextSplice: {
 		borderRightWidth: 1,
 		borderRightColor: '#0f1d4e',
-		paddingRight:5,
-		marginRight:5,
+		paddingRight: 5,
+		marginRight: 5,
 	}
 
 })
-const mapStateToProps=store=>{
+const mapStateToProps = store => {
 	return {
-		nav:store.nav,
-		userinfo:store.userinfo
+		nav: store.nav,
+		userinfo: store.userinfo
 	}
 }
 
